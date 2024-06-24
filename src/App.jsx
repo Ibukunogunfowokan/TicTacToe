62e1c9de8c91f78bc6708d73546bf31753e9cfd6
ibukunogunfowokan <ibukunogunfowokan@gmail.com>
2024-06-24 19:21:45 +0100

    Reapply "moves mapped"
    This reverts commit afd8ed1abd04376dd6153a427d0712f12eb1860f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,172 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+export default function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  
+  const [currentMove, setCurrentMove] = useState(0)
+  const currentSquare = history[currentMove] 
+  const isXnext = currentMove % 2 === 0;
 
-export default app(){
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXnext, setIsXnext] = useState(true)
-  function Square({ value, onSquareClick }) {
+  const handleOnplay = (newSquare)=>{
+    const newHistory = [...history.slice(0, currentMove + 1), newSquare];
+    setHistory(newHistory);
+    setCurrentMove(newHistory.length - 1)
+   
+    
+    
+  }
+
+  function Square({ value, handleClick }) {
     return (
-      <button className="square" onClick={onSquareClick}>
+      <button className="square" onClick={handleClick}>
         {value}
       </button>
     );
   }
-  
-  function Board({squares, isXnext, handleOnplay}) { 
-    const winner = calculateWinner(squares);
-    let status;
+
+  function Board({squareBox, isXnext, handleClick}) {
+    
+    const winner = calculateWinner(squareBox);
+    
+    let status ;
     if(winner){
-      status = 'Winner is: ' + winner;
+      status = `The winner is ${winner}`
       
     }else{
-      status = "Next player: "+ (isXnext ? 'X' :'O') 
+        status = `Next player: ${(isXnext ? 'X' : 'O')}`
     }
-  
-    function handleClick(i) {
-      const nextSquares = squares.slice();
-      if(nextSquares[i]|| calculateWinner(squares)){
-        return
-      }
+
+    const handleOnsquareClick = (i) => {
+
+      let newSquare = squareBox
      
-      if(isXnext){
-        nextSquares[i] = 'X';
-        
-      }else{
-        nextSquares[i] ='O'
-        
+
+      if (newSquare[i] || calculateWinner(squareBox)) {
+        return;
       }
+      if (isXnext) {
+        newSquare[i] = "X";
+      } else {
+        newSquare[i] = "O";
+      }
+      handleClick(newSquare)
       
-      setSquares(nextSquares);
-      setIsXnext(!isXnext);
-    }
-  
-    
-    function calculateWinner(squares) {
-      const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          return squares[a];
-        }
-      }
-      return null;
-    }
-  
-  
+    };
+
     return (
       <>
-        <div className='status'>{status}</div>
+      <div>{status}</div>
         <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square
+            value={squareBox[0]}
+            handleClick={() => {
+              handleOnsquareClick(0);
+            }}
+          />
+          <Square
+            value={squareBox[1]}
+            handleClick={() => {
+              handleOnsquareClick(1);
+            }}
+          />
+          <Square
+            value={squareBox[2]}
+            handleClick={() => {
+              handleOnsquareClick(2);
+            }}
+          />
         </div>
         <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square
+            value={squareBox[3]}
+            handleClick={() => {
+              handleOnsquareClick(3);
+            }}
+          />
+          <Square
+            value={squareBox[4]}
+            handleClick={() => {
+              handleOnsquareClick(4);
+            }}
+          />
+          <Square
+            value={squareBox[5]}
+            handleClick={() => {
+              handleOnsquareClick(5);
+            }}
+          />
         </div>
         <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          <Square
+            value={squareBox[6]}
+            handleClick={() => {
+              handleOnsquareClick(6);
+            }}
+          />
+          <Square
+            value={squareBox[7]}
+            handleClick={() => {
+              handleOnsquareClick(7);
+            }}
+          />
+          <Square
+            value={squareBox[8]}
+            handleClick={() => {
+              handleOnsquareClick(8);
+            }}
+          />
         </div>
       </>
     );
-  
-    
   }
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+function jumpToMove(nextMove){
+  setCurrentMove(nextMove)
   
-
-
-
-
-
-
-
-
-
-  return(
-    <>
-    <Board squares={} isXnext={} handleOnplay={}/>
-    </>
-  );  
 }
+  const moves = history.map((square, index)=>{
+    let description;
+    if(index > 0){
+      description = "return to move "+ index
+    }else{
+      description = "return to Start"
+    }
+
+    return (
+      <li key ={index}>
+        <button onClick={()=>{jumpToMove(index)}}>{description}</button>
+      </li>
+    )
+  })
+
+  return <>
+  <div className="game">
+    <div className="game-board">
+     <Board squareBox={currentSquare} isXnext={isXnext} handleClick={handleOnplay}/>
+    </div>
+    <div className="game-info">
+    <ol>{moves}</ol>
+    </div>
+  </div>
+
+  </>;
+}
+
